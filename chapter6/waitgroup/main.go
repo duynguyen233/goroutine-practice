e2ba0ea58c7de49a850ca82e5d2742b842057291
Minh Duy <minhduynguyen2303@gmail.com>
2025-02-03 16:04:28 +0700

    chapter 6: waitgroup

---
+++ chapter6/waitgroup/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type WaitGrp struct {
+	groupSize int
+	cond      *sync.Cond
+}
+
+func NewWaitGrp() *WaitGrp {
+	return &WaitGrp{
+		cond: sync.NewCond(&sync.Mutex{}),
+	}
+}
+
+func (wg *WaitGrp) Add(delta int) {
+	wg.cond.L.Lock()
+	wg.groupSize += delta
+	wg.cond.L.Unlock()
+}
+
+func (wg *WaitGrp) Wait() {
+	wg.cond.L.Lock()
+	for wg.groupSize > 0 {
+		wg.cond.Wait()
+	}
+	wg.cond.L.Unlock()
+}
+
+func (wg *WaitGrp) Done() {
+	wg.cond.L.Lock()
+	wg.groupSize--
+	if wg.groupSize == 0 {
+		wg.cond.Broadcast()
+	}
+	wg.cond.L.Unlock()
+}
+
+func DoWork(i int, wg *WaitGrp) {
+	fmt.Println("Doing task: ", i)
+	time.Sleep(1 * time.Second)
+	wg.Done()
+}
+
+func main() {
+	wg := NewWaitGrp()
+	wg.Add(15)
+	for i := 0; i < 15; i++ {
+		go DoWork(i, wg)
+	}
+	wg.Wait()
+	fmt.Println("All job is done")
+}
